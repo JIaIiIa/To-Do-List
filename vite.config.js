fc4e7afbe3dde85d60efc14c8d338ee52d3e608a
Loew Yablonsky <tryanotherhand@gmail.com>
2025-06-17 19:46:14 +0300

    download-2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: './To-Do-List',
   plugins: [react()],
   server: {
     open: true
