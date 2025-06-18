7b90e4b6eab0106e7d831f3837b998c9903b076a
Loew Yablonsky <tryanotherhand@gmail.com>
2025-06-18 14:05:53 +0300

    download-10

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'My App',
+      short_name: 'App',
+      start_url: '.',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#42a5f5',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ],
   server: {
     open: true
   }
